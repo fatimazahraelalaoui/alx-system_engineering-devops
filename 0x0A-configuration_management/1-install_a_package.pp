b1d68a050e77f8315c5df6a4ff26b4dbd8fc2474
fatimazahraelalaoui <elalaouifatimazahra02@gmail.com>
2023-11-24 11:34:44 -0800

    fr

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,11 @@
+##!/usr/bin/pup
+# Using Puppt to install flask from pip3
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
+
+package {'Werkzeug':
+  ensure   => '2.1.0',
+  provider => 'pip3'
+}
